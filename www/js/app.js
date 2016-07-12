3b2097379549c9954a9cbb803c196658b834cd6b
cellhone <okita@macbook.local>
2016-07-12 19:41:49 +0900

    Google Maps

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'FBAngular', 'ngStorage'])
+angular.module('starter', ['ionic', 'FBAngular', 'ngStorage', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -47,6 +47,15 @@ angular.module('starter', ['ionic', 'FBAngular', 'ngStorage'])
     views: {
       hoge: {
         templateUrl: 'templates/hoge.main.html',
+      }
+    }
+  })
+  .state('hoge.map', {
+    url: '/map',
+    views: {
+      map: {
+        templateUrl: 'templates/hoge.map.html',
+        controller: 'MapCtrl'
       }
     }
   })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -63,3 +63,45 @@ angular.module('starter')
 })
 
 
+.controller('MapCtrl', function($scope, $state, $cordovaGeolocation, $ionicLoading) {
+  var options = {timeout: 10000, enableHighAccuracy: true};
+  $ionicLoading.show();
+ 
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+ 
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+ 
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    
+    //Wait until the map is loaded
+    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+
+      var marker = new google.maps.Marker({
+          map: $scope.map,
+          animation: google.maps.Animation.DROP,
+          position: latLng
+      });      
+
+      var infoWindow = new google.maps.InfoWindow({
+          content: "Here I am!"
+      });
+
+      google.maps.event.addListener(marker, 'click', function () {
+        infoWindow.open($scope.map, marker);
+      });
+      
+      $ionicLoading.hide();
+
+    });
+ 
+  }, function(error){
+    console.log("Could not get location");
+    $ionicLoading.hide();
+  });
+});
